41715790713a382eb8a0c22557a037cf01d5804c
Duk000 <duk000@hotmail.com>
2023-09-15 14:19:14 -0500

    first commit

---
+++ src/main/java/com/foroAlura/app/AppApplication.java
@@ -0,0 +1,15 @@
+package com.foroAlura.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+
+		System.out.println("**************Hola Mundo Spring Boot**************");
+	}
+
+}
